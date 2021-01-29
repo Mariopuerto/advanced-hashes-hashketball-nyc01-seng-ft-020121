584aeaa46dba05a6735bad32e333a7edd8175cf2
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-29 19:59:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -164,16 +164,48 @@ def team_names
  end
 end
 
-def player_numbers(team_name)
-
+def player_numbers(input)
+  output = []
+  game_hash.each do |team, team_info|
+    if team_info[:team_name] == input 
+      team_info.each do |key, value|
+        if key == :players
+          value.each do |player|
+          output.push(player[:number])
+          end
+        end
+      end
+    end
+  end
+  return output
 end
 
-def player_stats(player)
-
+def player_stats(input)
+  game_hash.each do |team, team_info|
+    team_info.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if input == player[:player_name]
+            player.delete(:player_name) 
+            return player
+          end
+        end
+      end
+    end
+  end
 end
 
 def big_shoe_rebounds
-
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
 end
-
 # Write code here
