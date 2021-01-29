b200b3f9426e35cfade809b0301f4a9c07a6901b
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-29 19:26:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,13 +126,8 @@ def game_hash
   }
 end
 
-def num_points_scored(player_search)
-game_hash.each do |team, team_info|
-  team_info[:players].each do |player|
-    if player[:player_name] == player_search
-      return player[:points]
-    end
-  end
+def num_points_scored(player_name)
+find_player(player_name)[:points]
 end
 
 
